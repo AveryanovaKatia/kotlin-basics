bc5669bfd1ec0ff76415fada44a80cdd8da9c149
AveryanovaKatia <160598695+AveryanovaKatia@users.noreply.github.com>
2025-04-30 12:40:39 +0400

    type and println

--- src/topics/HelloWorld.kt
+++ src/topics/HelloWorld.kt
@@ -2,8 +2,15 @@ package topics
 
 /**
  * fun - для создания функций - методов
+ * $ - для обращения к переменной внутри строки
  */
 
 fun main() {
+
     println("HelloWorld!")
+
+    val count = 21
+    println("Hello $count people")
+
+    println("Sum is ${count + 3}")
 }
---
+++ src/topics/Variables.kt
@@ -0,0 +1,25 @@
+package topics
+
+/**
+ * Kotlin строго типилизированный язык
+ * Kotlin может предугадывать тип переменной
+ * val - константа
+ * var - переменная
+ */
+
+fun main() {
+
+    val test1: String = "Hello"
+
+    var test2: String = "Good morning"
+    test2 = "Good night"
+
+    /**
+     * равнозначная запись с
+     */
+
+    val test3 = "Hello"
+
+    var test4 = "Good morning"
+
+}
